acc1c6635f5a64496ea988f433feea11e44ff874
ocllLr <121541053+ocllLr@users.noreply.github.com>
2025-10-01 14:53:27 +0900

    Update script.js

--- prectice1/A/A4/script/script.js
+++ prectice1/A/A4/script/script.js
@@ -30,4 +30,12 @@ $(function(){
     tabCont.eq(index).show().siblings().hide();
   })
 
+  //팝업
+  $(".popup-btn").click(function(){
+    $(".popup-view").show()
+  })
+  $(".popup-close").click(function(){
+    $(".popup-view").hide()
+  })
+
 })
